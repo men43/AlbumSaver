1848076583914ea1829b27ab0b8c8d68135bf906
men43 <tmen43@gmail.com>
2014-06-21 16:07:02 +0300

    Implemented non-token mode

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ class utils(object):
 	def getScriptFolder():
 		return os.path.dirname(os.path.realpath(sys.argv[0])) + "\\"
 	def initLogging():
-		format = logging.Formatter("[%(asctime)s][%(levelname)s]  %(message)s", "%d-%m-%y, %H:%M:%S")
+		format = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s", "%d-%m-%y, %H:%M:%S")
 		logger = logging.getLogger("root")
 		logger.setLevel(20)
 		if options.OUTPUT_LOG != 0:
@@ -20,16 +20,12 @@ class utils(object):
 		output = logging.getLogger("root")
 		output.log(logLevel, message)
 		return
-	def readLinks():
-		links = open(options.LINKS_FILE, 'r')
-		result = links.readlines()
-		return result
 	def createConfig(filename):
 		config = configparser.ConfigParser()
 		config["setup"] = {"script_location": options.SCRIPT_LOCATION, "data_location": options.DATA_LOCATION,
 		"links_file": options.LINKS_FILE, "config_file": options.CONFIG_FILE, "log_file": options.LOG_FILE}
 		config["output"] = {"output_console": options.OUTPUT_CONSOLE, "output_log": options.OUTPUT_LOG}
-		config["token"] = {"access_token": options.ACCESS_TOKEN}
+		config["token"] = {"access_token": options.ACCESS_TOKEN, "use_token": options.USE_TOKEN}
 		with open(filename, 'w+') as configfile:
 			config.write(configfile)
 		return
@@ -50,11 +46,15 @@ class utils(object):
 		if "token" in config:
 			token = config["token"]
 			options.ACCESS_TOKEN = token["access_token"]
+			options.USE_TOKEN = token["use_token"]
 		return
+	def checkAccessToken(accessToken):
+		return False #TODO
+	def authorize(username, password):
+		return False #TODO
 	def apiGet(method, params):
 		request = urllib.request.Request("https://api.vk.com/method/"+method+"?"+params+"&access_token="+options.ACCESS_TOKEN)
-		page = urllib.request.urlopen(request)
-		return page.read().decode('cp1251')
+		return urllib.request.urlopen(request).read().decode('cp1251')
 	def downloadImage(url, filename):
 		request = urllib.request.Request(url)
 		image = urllib.request.urlopen(request)
@@ -72,6 +72,7 @@ class options(object):
 	OUTPUT_CONSOLE = 1
 	OUTPUT_LOG = 1
 	ACCESS_TOKEN = ""
+	USE_TOKEN = 1
 	
 def startup():
 	utils.initLogging()
@@ -81,6 +82,15 @@ def startup():
 	else:
 		utils.outputMessage(20, "No config file detected, creating one.")
 		utils.createConfig(options.CONFIG_FILE)
+	if options.ACCESS_TOKEN.strip() == "":
+		utils.outputMessage(30, "Can't find your access token, you can input it manually.")
+		accessToken = input("Token (press ENTER for non-token mode): ")
+		if accessToken.strip() == "":
+			utils.outputMessage(20, "Non-token mode enabled.")
+			options.ACCESS_TOKEN = ""
+			options.USE_TOKEN = 0
+		else: 
+			options.ACCESS_TOKEN = accessToken
 	if os.path.exists(options.SCRIPT_LOCATION+options.DATA_LOCATION) != True:
 		utils.outputMessage(20, "Can't find data folder, creating.")
 		os.mkdir(options.SCRIPT_LOCATION+options.DATA_LOCATION)
@@ -93,18 +103,29 @@ def startup():
 
 def run():
 	startup()
-	lines = utils.readLinks()
+	lines = open(options.LINKS_FILE, 'r').readlines()
 	for i in range(0, len(lines)):
 		raw = lines[i].split("album")
 		full = raw[1].split("_")
-		rawAlbumData = utils.apiGet("photos.get", "owner_id="+full[0]+"?&album_id="+full[1].rstrip("\n"))
-		rawAlbumName = utils.apiGet("photos.getAlbums", "owner_id="+full[0]+"?&album_id="+full[1].rstrip("\n")+"&rev=1")
+		try:
+			rawAlbumData = utils.apiGet("photos.get", "owner_id="+full[0]+"?&album_id="+full[1].rstrip("\n"))
+			rawAlbumName = utils.apiGet("photos.getAlbums", "owner_id="+full[0]+"?&album_ids="+full[1].rstrip("\n"))
+		except:
+			utils.outputMessage(50, "Can't connect to VK API. Check your internet connection. Terminating.")
+			sys.exit()
 		decodedJsonData = json.loads(rawAlbumData)
 		decodedJsonName = json.loads(rawAlbumName)
-		#os.mkdir(options.DATA_LOCATION+"/"+decodedJsonName["response"][0]["title"])
-		os.mkdir(options.DATA_LOCATION+"/"+str(i+1))
-		for w in range(0,len(decodedJsonData["response"])):
-			utils.downloadImage(decodedJsonData["response"][w]["src_big"], options.DATA_LOCATION+"/"+str(i+1)+"/"+str(w+1)+".jpg")
-		utils.outputMessage(20, "Finished "+str(i+1)+" album")
+		if options.USE_TOKEN != 0: albumFolder = decodedJsonName["response"][0]["title"].encode('cp1251').decode('utf-8')
+		if options.USE_TOKEN == 0: 
+			albumFolder = i+1
+		if "error" not in decodedJsonData:
+			os.mkdir(options.DATA_LOCATION+"/"+str(albumFolder))
+			for w in range(0,len(decodedJsonData["response"])):
+				utils.downloadImage(decodedJsonData["response"][w]["src_big"], options.DATA_LOCATION+"/"+str(albumFolder)+"/"+str(w+1)+".jpg")
+			utils.outputMessage(20, "Finished "+str(albumFolder)+" album, downloaded "+str(len(decodedJsonData["response"]))+" photos.")
+		else:
+			utils.outputMessage(50, "API error: "+decodedJsonData["error"]["error_msg"]+". Terminating.")
+			sys.exit()
+	utils.outputMessage(20, "Finished all albums, terminating.")
 	return
 run()
